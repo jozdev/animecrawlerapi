2b3c162190b66a4189c51d566989c94793f9a759
João Magalhães <58850639+jozdev@users.noreply.github.com>
2020-10-29 19:56:01 +0000

    Create server.js

---
+++ gogo-stream.com/server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser'),
+  apicache = require('apicache'),
+  cache = apicache.middleware;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cache('5 days'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var routes = require('./api/routes/routes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('gogo-stream.com server started on: ' + port);
